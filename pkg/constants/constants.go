3df0681dfc053b73c42a2952a1841c944d126243
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2023-08-02 11:10:34 -0300

    Add comments for constants

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,16 +1,17 @@
+// Package to aggregate default constants.
 package constants
 
 const (
-	WindowTitle         = "GO8EM"
-	MemorySize          = 0x1000
-	ProgramStartAddress = 0x200
-	ScreenWidth         = 64
-	ScreenHeight        = 32
-	ScreenScaleFactor   = 10
+	WindowTitle         = "GO8EM" // name title of the executable
+	MemorySize          = 0x1000  // upper memory limit
+	ProgramStartAddress = 0x200   // Chip-8 default starting address
+	ScreenWidth         = 64      // screen pixel width
+	ScreenHeight        = 32      // screen pixel height
+	ScreenScaleFactor   = 10      // default pixel unit scaling factor
 
-	CharacterSetStartAddress = 0x00
-	DefaultSpriteHeight      = 5
-	ROMMaxSize               = 0xE00
+	SpriteSetStartAddress = 0x00  // first address of sprite set
+	DefaultSpriteHeight   = 5     // default height of sprite to serve as index
+	ROMMaxSize            = 0xE00 // maximum ROM size to check limits
 
-	ClockFrequency = 700
+	ClockFrequency = 700 // default emulator clock frequency
 )
